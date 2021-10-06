b3561ac5d5c88f334b5579ceab48c9d48c22bba8
Tariqazan <tariqmolla8@gmail.com>
2021-10-06 22:33:15 +0600

    done

--- orange/js/carousel.js
+++ orange/js/carousel.js
@@ -32,29 +32,30 @@ jQuery("#carousel").owlCarousel({
 });
 
 
-jQuery(document).ready(function($) {
-    "use strict";
-    $('#customers-testimonials').owlCarousel({
-        loop: true,
-        center: true,
-        items: 3,
-        margin: 30,
-        autoplay: true,
-        dots: true,
-        nav: true,
-        autoplayTimeout: 8500,
-        smartSpeed: 450,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            1170: {
-                items: 3
-            }
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 2
         }
-    });
+    }
+})
+
+var selector = $('.owl-carousel');
+
+$('.my-next-button').click(function() {
+    selector.trigger('next.owl.carousel');
+});
+
+$('.my-prev-button').click(function() {
+    selector.trigger('prev.owl.carousel');
 });
--- orange/js/details-carousel.js
+++ orange/js/details-carousel.js
@@ -1,19 +1,17 @@
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-        loop: true,
-        nav: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 4
-            }
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
         }
-    });
-});
+    }
+})

925e48a6ea56e325abecaac395904764bc591284
Tariqazan <45086258+Tariqazan@users.noreply.github.com>
2021-10-07 12:47:01 +0600

    trying

--- blue/js/carousel.js
+++ blue/js/carousel.js
@@ -30,7 +30,38 @@ jQuery("#carousel").owlCarousel({
         }
     }
 });
+jQuery("#banner-carousel").owlCarousel({
+    autoplay: true,
+    rewind: true,
+    /* use rewind if you don't want loop */
+    margin: 20,
+    /*
+   animateOut: 'fadeOut',
+   animateIn: 'fadeIn',
+   */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
 
+        600: {
+            items: 1
+        },
+
+        1024: {
+            items: 1
+        },
+
+        1366: {
+            items: 1
+        }
+    }
+});
 
 jQuery(document).ready(function($) {
     "use strict";
--- orange/js/carousel.js
+++ orange/js/carousel.js
@@ -31,6 +31,39 @@ jQuery("#carousel").owlCarousel({
     }
 });
 
+jQuery("#banner-carousel").owlCarousel({
+    autoplay: true,
+    rewind: true,
+    /* use rewind if you don't want loop */
+    margin: 20,
+    /*
+   animateOut: 'fadeOut',
+   animateIn: 'fadeIn',
+   */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 1
+        },
+
+        1024: {
+            items: 1
+        },
+
+        1366: {
+            items: 1
+        }
+    }
+});
+
 
 $('.owl-carousel').owlCarousel({
     stagePadding: 50,
